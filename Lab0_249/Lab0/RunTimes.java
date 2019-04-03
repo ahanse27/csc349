369fe84de3b8f461ac8a7fc1c40d112e3d8b49e2
movvam <manikanthmovva@gmail.com>
2019-04-03 08:34:52 -0700

    finished Runtimes

--- Lab0_249/Lab0/RunTimes.java
+++ Lab0_249/Lab0/RunTimes.java
@@ -1,15 +1,16 @@
-import Algorithms;
+import Algorithms.java;
 
 public class RunTimes
 {
     public static void main(String args[])
         {
+            long startTime, endTime, runTime;
             System.out.println("Logarithmic algorithm's running times:");
 
-            for (int N = 1000; N<100000000; N++){
-               startTime = nanoTime();
-               logarithmicAlgorithm(N)
-               endTime = nanoTime();    
+            for (int N = 1000; N<100000000; N=N*2){
+               startTime = System.nanoTime();
+               logarithmicAlgorithm(N);
+               endTime = System.nanoTime();    
                runTime = (endTime-startTime)/(1000000);
                System.out.println("T(" + N + ") = " + runTime);
             }
@@ -17,13 +18,47 @@ public class RunTimes
             System.out.println("");
             System.out.println("Linear algorithm's running times:");
 
-            for (int N = 1000; N<100000000; N++){
-               startTime = nanoTime();
-               logarithmicAlgorithm(N)
-               endTime = nanoTime();
+            for (int N = 1000; N<100000000; N=N*2){
+               startTime = System.nanoTime();
+               linearAlgorithm(N);
+               endTime = System.nanoTime();
                runTime = (endTime-startTime)/(1000000);
                System.out.println("T(" + N + ") = " + runTime);
             } 
+
+            System.out.println("");
+            System.out.println("NlogN algorithm's running times:");
+
+            for (int N = 1000; N<100000000; N=N*2){
+               startTime = nanoTime();
+               NlogNAlgorithm(N);
+               endTime = nanoTime();
+               runTime = (endTime-startTime)/(1000000);
+               System.out.println("T(" + N + ") = " + runTime);
+            }
+ 
+            System.out.println("");
+            System.out.println("Quadratic algorithm's running times:");
+
+            for (int N = 1000; N<512000; N=N*2){
+               startTime = nanoTime();
+               quadraticAlgorithm(N);
+               endTime = nanoTime();
+               runTime = (endTime-startTime)/(1000000);
+               System.out.println("T(" + N + ") = " + runTime);
+            }
+
+
+            System.out.println("");
+            System.out.println("Cubic algorithm's running times:");
+
+            for (int N = 1000; N<8000; N=N*2){
+               startTime = nanoTime();
+               cubicAlgorithm(N);
+               endTime = nanoTime();
+               runTime = (endTime-startTime)/(1000000);
+               System.out.println("T(" + N + ") = " + runTime);
+            }
 
             return;
         }
