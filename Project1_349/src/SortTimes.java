f35f875323b82a01d12418ed68099d759fd6a9f8
Mani Movva <manikanthmovva@gmail.com>
2019-04-05 15:40:38 -0700

    add printing for SortTimes

--- Project1_349/src/SortTimes.java
+++ Project1_349/src/SortTimes.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class SortTimes {
 
     public static void main(String [] args){
@@ -8,12 +6,34 @@ public class SortTimes {
         int[] arr2 = new int[N];
         int[] arr3 = new int[N];
 
-        for (int i = 0; i < N; i++){
-            int n = (int)(Math.random()*N);
-            arr1[i] = n;
-            arr2[i] = n;
-            arr3[i] = n;
-            System.out.print(arr1[i]);
+        for (N = 5000; N <= 160000; N=N*2){
+            for (int t = 0; t < 5; t++){
+
+                for (int i = 0; i < N; i++){
+                    int n = (int)(Math.random()*N);
+                    arr1[i] = n;
+                    arr2[i] = n;
+                    arr3[i] = n;
+                }
+
+                long startTime = System.nanoTime();
+                Sorts.selectionSort(arr1, N);
+                long endTime = System.nanoTime();
+                long time1 = (endTime-startTime)/(1000000);
+
+                startTime = System.nanoTime();
+                Sorts.mergeSort(arr2, N);
+                endTime = System.nanoTime();
+                long time2 = (endTime-startTime)/(1000000);
+
+                startTime = System.nanoTime();
+                Sorts.quickSort(arr2, N);
+                endTime = System.nanoTime();
+                long time3 = (endTime-startTime)/(1000000);
+
+                System.out.println("N=" + N+": T_ss="+ time1+", T_ms="+ time2+", T_qs="+ time3 );
+            }
+            System.out.println();
         }
 
     }
