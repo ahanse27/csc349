3df815c65155c82b2604771c4c27736f5eceb704
ayakkala1 <ayakkala@calpoly.edu>
2019-04-05 11:46:26 -0700

    add more test cases

--- Project1_349/src/TestCases.java
+++ Project1_349/src/TestCases.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Assertions;
 public class TestCases {
     @Test
     public void TestSelectionSort1() {
-        int[] arr = {4, 9, 8, 7};
+        int[] arr = {4, 9, 8, 7, 4};
         Sorts.selectionSort(arr, arr.length);
-        Assertions.assertArrayEquals(new int[]{4, 7, 8, 9}, arr);
+        Assertions.assertArrayEquals(new int[]{4, 4, 7, 8, 9}, arr);
     }
     @Test
     public void TestSelectionSort2() {
@@ -15,15 +15,27 @@ public class TestCases {
         Assertions.assertArrayEquals(new int[]{1, 1, 1}, arr);
     }
     @Test
+    public void TestSelectionSort3() {
+        int[] arr = {};
+        Sorts.selectionSort(arr, arr.length);
+        Assertions.assertArrayEquals(new int[] {}, arr);
+    }
+    @Test
     public void TestMergeSort1() {
-        int[] arr = {4, 9, 8, 7};
+        int[] arr = {4, 9, 8, 7, 4};
         Sorts.mergeSort(arr, arr.length);
-        Assertions.assertArrayEquals(new int[]{4, 7, 8, 9}, arr);
+        Assertions.assertArrayEquals(new int[]{4, 4, 7, 8, 9}, arr);
     }
     @Test
     public void TestMergeSort2() {
         int[] arr = {1,1,1};
         Sorts.mergeSort(arr, arr.length);
         Assertions.assertArrayEquals(new int[] {1,1,1}, arr);
+    }
+    @Test
+    public void TestMergeSort3() {
+        int[] arr = {};
+        Sorts.mergeSort(arr, arr.length);
+        Assertions.assertArrayEquals(new int[] {}, arr);
     }
 }
