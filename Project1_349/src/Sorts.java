3f01c43378b960915298bc3441dfdb2168d1835c
ayakkala1 <ayakkala@calpoly.edu>
2019-04-05 11:28:52 -0700

    fix format of mergeSort to fit pseudocode

--- Project1_349/src/Sorts.java
+++ Project1_349/src/Sorts.java
@@ -14,50 +14,57 @@ public class Sorts {
             arr[minIndex] = temp;
         }
     }
-    public static void mergeSort(int[] arr, int N){
-        if (arr.length > 1){
-            int middle = arr.length / 2;
-            int[] first = new int[middle];
-            for (int i = 0; i < middle; i++){
-                first[i] = arr[i];
-            }
-            int[] last = new int[N - middle];
-            for (int i = 0; i < N - middle; i++){
-                last[i] = arr[middle + i];
-            }
-            mergeSort(first,first.length);
-            mergeSort(last, last.length);
-
-            int i = 0;
-            int j = 0;
-            int k = 0;
-
-            while (i < middle &&  j < N - middle){
-                if (first[i] <= last[j])
-                {
-                    arr[k] = first[i];
-                    i = i + 1;
-                }
-                else
-                {
-                    arr[k] = last[j];
-                    j = j + 1;
-                }
-                k = k + 1;
-            }
-            while (i < middle)
-            {
-                arr[k] = first[i];
-                i = i + 1;
-                k = k + 1;
-            }
-
-            while (j < N - middle)
-            {
-                arr[k] = last[j];
-                j = j + 1;
-                k = k + 1;
-            }
+    public static void mergeSort(int[] list, int N){
+        mergeSort(list, 0, N-1);
+    }
+    private static void mergeSort (int[] list,  int first,  int last) {
+        if (first < last) {
+            int middle = (first + last) / 2;
+            mergeSort(list, first, middle);
+            mergeSort(list, middle + 1, last);
+            mergeSortedHalves(list, first, middle , last);
         }
     }
+    private static void mergeSortedHalves (int[]  arr, int left, int middle, int right){
+        int[] temp = new int[right - left + 1];
+
+        int index1 = left;
+        int index2 = middle + 1;
+        int index = 0;
+
+        while (index1 < middle + 1 &&  index2 < right + 1){
+            if (arr[index1] <= arr[index2])
+            {
+                temp[index] = arr[index1];
+                index1 = index1 + 1;
+            }
+            else
+            {
+                temp[index] = arr[index2];
+                index2 = index2 + 1;
+            }
+            index = index + 1;
+        }
+
+        while (index1 < middle + 1)
+        {
+            temp[index] = arr[index1];
+            index1 = index1 + 1;
+            index = index + 1;
+        }
+
+        while (index2 < right + 1)
+        {
+            temp[index] = arr[index2];
+            index2 = index2 + 1;
+            index = index + 1;
+        }
+
+        int k = 0;
+        for (int i = left; i < right + 1; i++){
+            arr[i] = temp[k];
+            k = k + 1;
+        }
+    }
+
 }
--- Project1_349/src/TestCases.java
+++ Project1_349/src/TestCases.java
@@ -2,20 +2,20 @@ import java.util.Arrays;
 
 public class TestCases {
     public static void main(String[] args){
-        int[] arr1 = {10,9,8};
-        Sorts.selectionSort(arr1, 3);
+        int[] arr1 = {4,9,8,7};
+        Sorts.selectionSort(arr1, arr1.length);
         System.out.println(Arrays.toString(arr1));
 
         int[] arr2 = {1,1,1};
-        Sorts.selectionSort(arr2, 3);
+        Sorts.selectionSort(arr2, arr2.length);
         System.out.println(Arrays.toString(arr2));
 
-        int[] arr3 = {10,9,8};
-        Sorts.mergeSort(arr3, 3);
+        int[] arr3 = {4,9,8,7};
+        Sorts.mergeSort(arr3, arr3.length);
         System.out.println(Arrays.toString(arr3));
 
         int[] arr4 = {1,1,1};
-        Sorts.mergeSort(arr4, 3);
+        Sorts.mergeSort(arr4, arr4.length);
         System.out.println(Arrays.toString(arr4));
     }
 }
