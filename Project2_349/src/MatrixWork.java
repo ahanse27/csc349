b3e64f1d8b36111cdab0d8fec38274b29ad6be5f
Mani Movva <manikanthmovva@gmail.com>
2019-04-15 08:52:40 -0700

    add matrix size check

--- Project2_349/src/MatrixWork.java
+++ Project2_349/src/MatrixWork.java
@@ -1,2 +1,10 @@
 public class MatrixWork {
+    public static int[][] matrixProduct (int [][] A, int[][] B){
+
+        if (B.length != A[0].length){ //check that mat sizes are valid
+            throw new IllegalArgumentException();
+        }
+
+        return A;
+    }
 }
