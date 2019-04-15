9ea96658a2050a7b6127e0d9fd7470ac85171de6
ayakkala1 <ayakkala@calpoly.edu>
2019-04-15 08:53:35 -0700

    Working on reading file

--- Project2_349/src/MatrixWork.java
+++ Project2_349/src/MatrixWork.java
@@ -1,2 +1,19 @@
-public class MatrixWork {
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+
+public class MatrixWork{
+    public static int[][]matrixProduct(int[][]A, int[][]B) throws IllegalArgumentException, FileNotFoundException {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the file name");
+        String filename = in.nextLine();
+        File file = new File(filename);
+        Scanner sc = new Scanner(file);
+        int counter = 0;
+        while (sc.hasNextLine()){
+            if (counter == 0){
+
+            }
+        }
+    }
 }
