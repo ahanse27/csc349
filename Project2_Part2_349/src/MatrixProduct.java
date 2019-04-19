4a97eea809c0cdfd89238191d4a83bab327bf56c
ayakkala1 <ayakkala@calpoly.edu>
2019-04-19 08:13:54 -0700

    methods init

--- Project2_Part2_349/src/MatrixProduct.java
+++ Project2_Part2_349/src/MatrixProduct.java
@@ -1,2 +1,9 @@
 public class MatrixProduct {
+    public static int[][]matrixProduct_DAC(int[][]A, int[][] B){
+        return new int[1][1];
+    }
+    public static  int[][]  matrixProduct_Strassen(int[][] A,int[][] B){
+        return new int[1][1];
+    }
+
 }
