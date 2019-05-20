19a2fefcbd81a43f359d4254dc955645fc98570e
ayakkala1 <ayakkala@calpoly.edu>
2019-05-20 08:36:24 -0700

    DiGraph class rough

--- Project5_349/src/DiGraph.java
+++ Project5_349/src/DiGraph.java
@@ -1,2 +1,51 @@
+import java.util.LinkedList;
+
 public class DiGraph {
+    private LinkedList<Integer>[] Adj;
+
+    public void DiGraph(int N){
+        this.Adj = new LinkedList[N];
+        for (int i = 0; i < N; i++){
+            Adj[i].add(i);
+        }
+    }
+    public void addEdge(int from, int to){
+        if (!Adj[from - 1].contains(to - 1)){
+            Adj[from - 1].add(to - 1);
+        }
+    }
+    public void deleteEdge(int from, int to){
+        int toIndex = Adj[from - 1].indexOf(to - 1);
+        if (toIndex != -1){
+            Adj[from - 1].remove(toIndex);
+        }
+    }
+    public int edgeCount(){
+        int sum = 0;
+        for (LinkedList<Integer> ll : Adj){
+            sum = sum + ll.size() - 1;
+        }
+        return sum;
+    }
+    public int vertexCount(){
+        return Adj.length;
+    }
+    public void print(){
+        for (int i = 0; i < Adj.length; i++){
+            System.out.printf("" + (i + 1) + "is connected to:");
+            if (Adj[i].size() > 1) {
+                for (int j = 1; j < Adj[i].size(); j++) {
+                    if (j == 1) {
+                        System.out.printf(" " + Adj[i].get(j));
+                    }
+                    else{
+                        System.out.printf(", " + Adj[i].get(j));
+                    }
+                }
+            }
+            if (i != Adj.length - 1) {
+                System.out.printf("\n");
+            }
+        }
+    }
 }
