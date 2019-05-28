b32f226e43af6396381f98b8575ee7e28e1e5f7f
ayakkala1 <ayakkala@calpoly.edu>
2019-05-28 08:48:28 -0700

    demo formatting and cases

--- Project5_349/src/DiGraphTest.java
+++ Project5_349/src/DiGraphTest.java
@@ -16,27 +16,31 @@ public class DiGraphTest {
                 "- Quit (enter q)");
 
         String code = in.next();
+        String line = in.nextLine();
         while (1 == 1) {
+            if (line.length() > 1){
+                code = "l";
+            }
             switch (code){
                 case "a":
-                    System.out.println("Enter start vertex");
+                    System.out.println("\nEnter start and destination vertex");
                     int from = Integer.parseInt(in.next());
-                    System.out.println("Enter destination vertex");
+                    //System.out.println("Enter destination vertex");
                     int to = Integer.parseInt(in.next());
                     test.addEdge(from,to);
-                    System.out.println("\nAdded edge (" + from + ", " + to + ")");
+                    System.out.println("\n(" + from + ", " + to + ")" + " is now added to the graph");
                     break;
                 case "d":
-                    System.out.println("Enter start vertex");
+                    System.out.println("\nEnter start and destination vertex");
                     from = in.nextInt();
-                    System.out.println("Enter destination vertex");
+                    //System.out.println("Enter destination vertex");
                     to = in.nextInt();
                     test.deleteEdge(from,to);
                     System.out.println("\nDeleted edge (" + from + ", " + to + ")");
                     break;
                 case "e":
-                    int e = test.edgeCount();
-                    System.out.println("\nThe number of edges is " + e);
+                    int edge = test.edgeCount();
+                    System.out.println("\nThe number of edges is " + edge);
                     break;
                 case "v":
                     int v = test.vertexCount();
@@ -47,17 +51,29 @@ public class DiGraphTest {
                     test.print();
                     break;
                 case "t":
-                    int[] sortedList = test.topSort();
-                    for (int i = 0; i < sortedList.length - 1; i++){
-                        System.out.printf("" + sortedList[i] + ", ");
+                    System.out.println("\nTopologically sorted list");
+                    try {
+                        int[] sortedList = test.topSort();
+                        for (int i = 0; i < sortedList.length - 1; i++) {
+                            System.out.printf("" + sortedList[i] + ", ");
+                        }
+                        System.out.printf("" + sortedList[sortedList.length - 1]);
+                        System.out.println();
                     }
-                    System.out.printf("" + sortedList[sortedList.length - 1]);
-                    System.out.println();
+                    catch(IllegalArgumentException e){
+                        System.out.println("Graph is cyclic!");
+                    }
                     break;
                 case "q":
+                    System.out.println("Good bye.");
                     System.exit(0);
+                default:
+                    System.out.println("Not a valid input!");
             }
             code = in.next();
+            if (in.hasNextLine()) {
+                line = in.nextLine();
+            }
         }
     }
 
