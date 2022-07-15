811b4cc3074ccd163339fe11aa190a85b5404a70
ahanse27 <91102191+ahanse27@users.noreply.github.com>
2022-07-15 11:05:43 -0700

    Update ChangeMaker.java

--- Project4_349/src/ChangeMaker.java
+++ Project4_349/src/ChangeMaker.java
@@ -1,120 +1,129 @@
-//Mani Movva (mmovva@calpoly.edu) and Anish Yakkala (ayakkala@calpoly.edu) Date: 05/24/19 Assignment: Project 4
-
 import java.util.Scanner;
 
 public class ChangeMaker {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("\nEnter the number of coin-denominations and the set of coin values:");
-        int k = in.nextInt();
-        int[] d = new int[k];
-        int counter = 0;
-        while (counter < k) {
-            d[counter] = in.nextInt();
-            counter++;
-        }
-        while (1 == 1) {
-            System.out.println("\nEnter a positive amount to be changed (enter 0 to quit):");
-            int n = in.nextInt();
-            if (n == 0) {
-                System.out.println("Thanks for playing. Good Bye.");
-                System.exit(0);
-            }
-            int[] counts_DP = change_DP(n, d);
-            int[] counts_greedy = change_greedy(n, d);
 
-            //printing for DP
-            System.out.println("\nDP algorithm results");
-            System.out.println("Amount: " + n);
-            System.out.printf("Optimal distribution: ");
-            int sum = 0;
-            for (int i = 0; i < d.length; i++) {
-                if (counts_DP[i] > 0) {
-                    if (sum > 0) {
-                        System.out.printf(" + " + counts_DP[i] + "*" + d[i] + "c");
-                    } else {
-                        System.out.printf("" + counts_DP[i] + "*" + d[i] + "c");
-                    }
-                    sum = sum + counts_DP[i];
-                }
-            }
-            System.out.println("\nOptimal coin count: " + sum);
+    public static int[] change_DP(int n, int[] d){  /* n is the amount and d is the array for the coin-values */
 
-            //printing for greedy
-            System.out.println("\nGreedy algorithm results");
-            System.out.println("Amount: " + n);
-            System.out.printf("Optimal distribution: ");
-            sum = 0;
-            for (int i = 0; i < d.length; i++) {
-                if (counts_greedy[i] > 0) {
-                    if (sum > 0) {
-                        System.out.printf(" + " + counts_greedy[i] + "*" + d[i] + "c");
-                    } else {
-                        System.out.printf("" + counts_greedy[i] + "*" + d[i] + "c");
-                    }
-                    sum = sum + counts_greedy[i];
-                }
-            }
-            System.out.println("\nOptimal coin count: " + sum);
-        }
-    }
+        int e = d.length;
+        int [] A = new int[n]; // 1 to n
+        int [] B = new int[e]; //count of coins (di-denomination) for the given n
+        int [] C = new int[n]; // the remainder of our coins to compute through
 
-    public static int[] change_DP(int n, int[] d) {
-        int[] C = new int[n];
-        int[] A = new int[n];
         C[0] = 1;
-        A[0] = d.length - 1;
-        for (int i = 2; i <= n; i++){
-            int min = 9999;
-            for (int j = 0; j < d.length; j++){
-               int remainder = i - d[j];
-               if (remainder == 0){
-                   min = 0;
-                   C[i - 1] = 1;
-                   A[i - 1] = j;
-               }
-               else if (remainder > 0) {
-                   int count = 1;
-                   int added = C[remainder - 1];
-                   if (count + added < min) {
-                       min = count + added;
-                       C[i - 1] = min;
-                       A[i - 1] = j;
-                    }
-                }
+        A[0] = e - 1;
 
+        for (int j = 2; j <= n; j++){ //
+            int minCoin = 500000; //max value of coin integer
+            for (int i = 0; i < e; i++){ //going the length of di - 5 (100, 25, 10, 5, 1)
+                int remainder = j - d[i];
+                if(remainder == 0){
+                    minCoin = 0;
+                    C[j - 1] = 1;
+                    A[j - 1] = i;
+                }
+                else if(remainder > 0){
+                    int count = 1;
+                    int added = C[remainder - 1];
+                    if(count + added < minCoin){
+                        minCoin = count + added;
+                        C[j - 1] = minCoin;
+                        A[j - 1] = i;
+                    }
+        
+                }
             }
         }
-        int[] counts = new int[d.length];
         int next_coin = A[n-1];
-        counts[next_coin] = counts[next_coin] + 1;
+        B[next_coin] = B[next_coin] + 1;
         int count_down = n - d[next_coin];
         while(count_down > 0){
             next_coin = A[count_down - 1];
-            counts[next_coin] = counts[next_coin] + 1;
+            B[next_coin] = B[next_coin] + 1;
             count_down = count_down - d[next_coin];
-        }
-        return counts;
+         }
+        return B;
     }
 
     public static int[] change_greedy(int n, int[] d){
-        int[] counts = new int[d.length];
-        int remaining = n;
-        int coin = 0;
-        while (remaining > 0){
-            while(coin < d.length-1){
-                // find the highest denomination coin less than or equal to the remaining amount
-                if (d[coin] <= remaining) break;
-                else{
-                    coin = coin+1;
-                }
-            }
+        int e = d.length;
+        int [] A = new int[n]; // 1 to n
+        int [] B = new int[e]; //count of coins (di-denomination) for the given n
+        int [] C = new int[n]; // the remainder of our coins to compute through
 
-            counts[coin] = remaining/d[coin];;
-            remaining = remaining % d[coin];
-        }
+        C[0] = 1;
+        A[0] = e - 1;
 
-        return counts;
+
+
+        return B;
     }
 
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the total number of coins and the set of coin values in decreasing order.\n");
+        System.out.println("Separate each new number by a space");
+        /*
+          input should look like: 5 100 25 10 5 1
+          k = 5 (#of coins), d = 100 25 10 5 1 (value of each coin)
+         */
+        int k = in.nextInt();
+        int[] d = new int[k];
+        for (int counter = 0; counter < k; counter++){
+            d[counter] = in.nextInt();
+        }
+        int n;
+        
+        int loopcount = 0;
+        while (loopcount < k){ //repeat while n's value is positive
+            System.out.println("Enter the total positive value of change needed or 0 to quit.");
+            n = in.nextInt();
+            if(n == 0){
+                System.out.println("Zero has been entered and the program has ended.");
+                System.exit(0);        /* purposeful exit */
+            }
+            int [] ch = change_DP(n, d);
+            int [] chg = change_greedy(n, d);
+            loopcount++;
+            /* printing for change_dp */
+            System.out.println("DP algorithm results");
+            System.out.println("Amount: " + n);          
+            System.out.print("Optimal distribution: " );
+            int distr = 0;
+            for(int l = 0; l < d.length; l++){
+                if(ch[l] > 0){
+                    if(distr > 0){
+                        System.out.print(" + " + ch[l] + "*" + d[l] + "c");
+                    }
+                    else{
+                        System.out.print("" + ch[l] + "*" + d[l] + "c");
+                    }
+                }
+                distr = distr + ch[l];
+            }
+            System.out.print("\n");
+            System.out.println("Optimal coin count: " + distr);
+
+            
+            /* printing for change_greedy */
+            System.out.println("Greedy algorithm results");
+            System.out.println("Amount: " + n);          
+            System.out.print("Optimal distribution: " );
+            int distr2 = 0;
+            for(int l = 0; l < d.length; l++){
+                if(ch[l] > 0){
+                    if(distr2> 0){
+                        System.out.print(" + " + chg[l] + "*" + d[l] + "c");
+                    }
+                    else{
+                        System.out.print("" + chg[l] + "*" + d[l] + "c");
+                    }
+                }
+                distr2 = distr2 + chg[l];
+            }
+            System.out.print("\n");
+            System.out.println("Optimal coin count: " + distr2);
+
+        }
+
+    }
 }
